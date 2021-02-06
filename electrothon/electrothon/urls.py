97350167d53325aa75509f20242d9db41c195127
Agrim <agrim3502@gmail.com>
2021-02-06 22:56:46 +0530

    Bugs resolved

--- electrothon/electrothon/urls.py
+++ electrothon/electrothon/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from . import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    path('accounts/',include('accounts.urls')),
     path('',views.index,name = 'index'),
     path('admin/', admin.site.urls),
 ]
