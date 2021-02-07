ebd3324bdd473ca0034ae043c538a0eeb47711c4
Agrim <agrim3502@gmail.com>
2021-02-07 11:30:36 +0530

    lend, borrow and donate

--- electrothon/inventory/urls.py
+++ electrothon/inventory/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns =  [
     path('borrow',views.borrow,name='borrow'),
     path('profile',views.profile,name='profile'),
-    path('lend',views.lend,name='lend')
+    path('lend',views.lend,name='lend'),
+    path('donate',views.donate,name='donate')
 ]
